eaa89da195ab58c9bea26a6fae19436049c85dca
Mike Ellis <git@mikeellis.org.uk>
2020-06-06 15:48:10 +0100

    Modified to support two I2C sensors

--- THPpoll.py
+++ THPpoll.py
@@ -1,6 +1,7 @@
 import time
 import sys
 import requests
+import traceback
 
 import bme280    # Read the BME sensor
 
@@ -8,17 +9,19 @@ database   = 'http://192.168.16.1:8086/write?db=temperature'
 sampleTime = 10
 
 # Change the log location here
-location   = 'outside'
-sublocn    = 'upstairs'
+sensors = { 0x76: {'location':'outside', 'sublocn':'upstairs'},
+            0x77: {'location':'office',  'sublocn':'room'},
+            }
 
 def main():
     while True:
         try:
-            temperature,pressure,humidity = bme280.readBME280All()
             measurements  = ''
-            measurements += 'temperature,location={0},type={1} value={2}\n'.format(location, sublocn, temperature)
-            measurements += 'humidity,location={0},type={1} value={2}\n'.format(location, sublocn, humidity)
-            measurements += 'pressure,location={0},type={1} value={2}\n'.format(location, sublocn, pressure)
+            for id, locn in sensors.items():
+                temperature,pressure,humidity = bme280.readBME280All(addr=id)
+                measurements += 'temperature,location={0},type={1} value={2}\n'.format(locn['location'], locn['sublocn'], temperature)
+                measurements +=    'humidity,location={0},type={1} value={2}\n'.format(locn['location'], locn['sublocn'], humidity)
+                measurements +=    'pressure,location={0},type={1} value={2}\n'.format(locn['location'], locn['sublocn'], pressure)
 
             #print('\nSending to {}\n{}'.format(database,measurements))
             response = requests.post(database, data=measurements)
@@ -27,7 +30,8 @@ def main():
             time.sleep(sampleTime)
 
         except Exception as e:
-            print("Unexpected error:", sys.exc_info()[0])
+            track = traceback.format_exc()
+            print("Unexpected error:", track)
             break
             
 if __name__ == '__main__':
